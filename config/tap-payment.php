78631b5570eeb28556778f5f84df7f8645489b2d
AmrMohamedMesbah <amr.mesbah@creatifyteam.com>
2024-11-26 00:44:48 +0200

    create payment transactions structure and handle taps config

---
+++ config/tap-payment.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'secret_key' => env('TAP_PAYMENT_SECRET_KEY'),
+    'public_key' => env('TAP_PAYMENT_PUBLIC_KEY'),
+    'api_url' => env('TAP_PAYMENT_API_URL'),
+    'api_version' => env('TAP_PAYMENT_API_VERSION'),
+    'merchant_id' => env('TAP_PAYMENT_MERCHANT_ID'),
+    'api_key' => env('TAP_PAYMENT_API_KEY'),
+];

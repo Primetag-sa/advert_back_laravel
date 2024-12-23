662b54269f318dcff6a29095fbf5fcb6712065fd
muhammadali <muhammad.alfaqoui@gmail.com>
2024-12-23 21:39:33 +0300

    Merge branch 'fix/auth'

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,8 +13,8 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-//            UsersTableSeeder::class,
-//            AgenciesTableSeeder::class,
+        //    UsersTableSeeder::class,
+        //    AgenciesTableSeeder::class,
             FeaturesSeeder::class,
             PlansSeeder::class,
         ]);
